e510d58a9160d53b916037f7f9380c4bd1e8c6c0
Eden Chambers <EdenChambers5@gmail.com>
2018-09-11 12:22:25 -0600

    Gave my Monster a Name with "this."

--- src/model/MarshmallowMonster.java
+++ src/model/MarshmallowMonster.java
@@ -2,12 +2,27 @@ package model;
 
 public class MarshmallowMonster
 {
-
+	//--> Declaration Section <--
+	private String name; 
+	private double legCount; 
+	private int eyeCount; 
+	private boolean hasNoses; 
+	private int armCount;
+	
+	
 	public MarshmallowMonster()
 	{
 		//Default constructor (generic, not as cool)
 		//Default constructor initializes stuff. 
 		// Generally is boring and isn't as helpful. 
+	}
+	
+	public MarshmallowMonster(String name)
+	{ 
+		this.name = name; 
+				//this talks about the current object
+				// How to assign a parameter into an instance. 
+				
 	}
 	
 	public String toString()

73c39707ac3e482e9067859c4c69543e6199c836
Eden Chambers <EdenChambers5@gmail.com>
2018-09-11 12:13:18 -0600

    Look at and Throw Away Monster

---
+++ src/model/MarshmallowMonster.java
@@ -0,0 +1,19 @@
+package model;
+
+public class MarshmallowMonster
+{
+
+	public MarshmallowMonster()
+	{
+		//Default constructor (generic, not as cool)
+		//Default constructor initializes stuff. 
+		// Generally is boring and isn't as helpful. 
+	}
+	
+	public String toString()
+	{
+		String description = ""; 
+		
+		return description; 
+	}
+}
--- src/monster/MController.java
+++ src/monster/MController.java
@@ -1,10 +1,22 @@
 package monster;
 
+import model.MarshmallowMonster; 
+
 public class MController
 {
+	//Data member/Declaration section
+	// Without "new" (initializing) your program has nothing "noll" (kills the program)
+	private MarshmallowMonster myMonster; 
+	
+	//Constructors initialize data members
+	public MController()
+	{
+		myMonster = new MarshmallowMonster();
+	}
+	
 	public void start() 
 	{
-		
+		System.out.println(myMonster);
 	}
 	
 	

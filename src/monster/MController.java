2f2b917bed6f539de9ca1ee0b1a186c1c5a5ffc7
Eden Chambers <EdenChambers5@gmail.com>
2018-09-11 11:47:06 -0600

    Made a Project & Added it ot GitHub

---
+++ src/monster/MController.java
@@ -0,0 +1,11 @@
+package monster;
+
+public class MController
+{
+	public void start() 
+	{
+		
+	}
+	
+	
+}
---
+++ src/monster/MRunner.java
@@ -0,0 +1,11 @@
+package monster;
+
+public class MRunner
+{
+
+	public static void main (String [] args)
+	{
+		MController app = new MController (); 
+		app.start();
+	}
+}
